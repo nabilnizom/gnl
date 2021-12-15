f387d9dfdd8e3f198d161c6c3526e550ef6e49a6
Ahmad Nabil <unclebrendi@Ahmads-MacBook-Pro.local>
2021-12-15 14:51:07 +0800

    GNL 5.0 - added NUL terminator to buffer rd

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: amohd-ni <amohd-ni@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 00:18:36 by amohd-ni          #+#    #+#             */
-/*   Updated: 2021/12/08 00:18:37 by amohd-ni         ###   ########.fr       */
+/*   Updated: 2021/12/15 14:49:44 by amohd-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include "get_next_line.h"
 
 char	*ft_strcpy_ur(char	*dst, char	*src, int j)
@@ -30,10 +31,11 @@ char	*ft_realloc(char *str, size_t size)
 	new = malloc(sizeof(char) * size);
 	if (str)
 		new = ft_strdup(str);
+	free (str);
 	return (new);
 }
 
-char	*ft_strlcpy_l(char *dst, char *src, int i)
+char	*ft_strcpy_l(char *dst, char *src, int i)
 {
 	int	j;
 
@@ -44,6 +46,10 @@ char	*ft_strlcpy_l(char *dst, char *src, int i)
 			dst = ft_realloc(dst, (ft_strlen(dst) + BUFFER_SIZE));
 		dst[i++] = src[j++];
 	}
+	if (src[j] == '\n')
+		dst[i] = src[j];
+	i++;
+	dst[i] = '\0';
 	return (dst);
 }
 
@@ -52,9 +58,13 @@ int	find_nl(char	*str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\n' && str[i])
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
 		i++;
-	return (i);
+	}
+	return (-1);
 }
 
 char	*get_next_line(int fd)
@@ -62,23 +72,22 @@ char	*get_next_line(int fd)
 	char		*ret;
 	static char	*unret = NULL;
 	char		*rd;
-	int			i;
 
-	i = -1;
 	if (!unret)
-		unret = malloc(sizeof(char) * BUFFER_SIZE);
-	ret = malloc(sizeof(char) * BUFFER_SIZE + 1);
+		unret = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	ret = malloc(sizeof(char) * BUFFER_SIZE + 2);
 	ft_bzero(ret, BUFFER_SIZE + 1);
-	while (unret[++i])
-		ret[i] = unret[i];
-	ft_bzero(unret, BUFFER_SIZE);
-	rd = malloc(sizeof(char) * BUFFER_SIZE);
+	if (unret)
+		ret = ft_strcpy_ur(ret, unret, 0);
+	ft_bzero(unret, BUFFER_SIZE + 1);
+	rd = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	rd[BUFFER_SIZE] = '\0';
 	while (read(fd, rd, BUFFER_SIZE) > 0)
 	{
-		ret = ft_strlcpy_l(ret, rd, ft_strlen(ret));
-		if (rd[find_nl(rd)] == '\n')
-			unret = ft_strcpy_ur(unret, rd, find_nl(rd) + 1);
-		if (rd[find_nl(rd)] == '\n')
+		ret = ft_strcpy_l(ret, rd, ft_strlen(ret));
+		if (find_nl(rd) > 0)
+			unret = ft_strcpy_ur(unret, rd, find_nl(rd));
+		if (find_nl(rd) >= 0)
 			break ;
 	}
 	free (rd);
